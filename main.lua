8dea833f0bb8ba04c3a0047436694a91dca57703
Zach Wheeler <zwheeler2006@gmail.com>
2024-04-03 21:22:40 -0400

    added image for background

--- main.lua
+++ main.lua
@@ -1,13 +1,14 @@
 -- https://github.com/Ulydev/push
 	push = require 'push'
 
-	screen_width = 432 --1280
-	screen_height = 243 --720
+	screen_width = 1280 --1280
+	screen_height = 720 --720
 
-	vWindow_width = 432
-	vWindow_height = 243
+	vWindow_width = 1280
+	vWindow_height = 720
 
 	player_speed = 200
+
 
 function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
@@ -30,6 +31,8 @@ function love.load()
 	playerXPos = 100
 	playerYPos = 100
 
+    my_background = love.graphics.newImage('img_hero_header_background.jpg')
+
 	love.window.setTitle("School Game!")
 	love.window.setMode(screen_width, screen_height)
 end
@@ -44,7 +47,7 @@ function love.update(dt)
 	if love.keyboard.isDown('a') then
 		playerXPos = math.max(0, playerXPos + -player_speed * dt)
 	elseif love.keyboard.isDown('d') then
-		playerXPos = math.min(vWindow_height - 15, playerXPos + player_speed * dt)
+		playerXPos = math.min(vWindow_width - 5, playerXPos + player_speed * dt)
 	end
 end
 
@@ -57,9 +60,14 @@ end
 
 function love.draw()
 
+	love.graphics.setColor(1,1,1,1)
+
+	love.graphics.draw(my_background)
+
+
 	push:apply('start')
 
-	love.graphics.clear(40/255,45/255,52/255,255/255)
+	-- love.graphics.clear(40/255,45/255,52/255,255/255)
 
 	if gameState == 'start' then
 		love.graphics.printf('Start State', 0, 20, vWindow_width, 'center')
@@ -71,6 +79,3 @@ function love.draw()
 
 	push:apply('end')
 end
--- function love.draw()
--- 	love.graphics.print("Hello Keira", 400, 300)
--- end
